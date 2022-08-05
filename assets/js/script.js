9899e2a316620fe71d66ca8e58de0e9d5ab1645e
Nicholas Bales <nickb96@icloud.com>
2022-08-04 22:17:05 -0400

    edits made to clean up file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,3 @@
-// variable for characters (separate for each prompt?)
-// variable for user choice
-// variable for output
-// RNG for password function
-// function to take data and return password
-// 
-// prompt for criteria (length[8-128], THEN lowercase, capitals, numbers, special characters)
-// Output password to box 
-// Ensure click button works 
-// 
-
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 var passText = "";
@@ -22,8 +10,8 @@ function generatePassword() {
   var passPot = []
 
   var passLength = prompt("Please select a password length between 8 and 128 characters");
-  if (passLength < 8 && passLength > 128) {
-    passLength = prompt("Please enter a valid number.");
+  if (passLength < 8 || passLength > 128) {
+    passLength = alert("Please enter a valid number.");
     return
   } else {
     var upperChoice = confirm("Would you like uppercase letters?");
@@ -56,13 +44,10 @@ function generatePassword() {
   
 }
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
-// }
-// THIS IS THE LINE TELLING FUNCTION TO RUN WHEN CLICKED
 generateBtn.addEventListener("click", writePassword);
